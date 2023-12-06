ea4ad76ee4ef2d04e5b6baa915b99d88bc4cbed4
Derya Acar <118841541+deryaxacar@users.noreply.github.com>
2023-12-06 17:23:04 +0300

    Create ft_printf2.c

---
+++ ft_printf2.c
@@ -0,0 +1,52 @@
+#include <stdarg.h>
+#include <unistd.h>
+
+void	ft_putstr(char *str)
+{
+	if (!str) str = "(null)";
+	write(1, str, strlen(str));
+}
+
+int ft_putnbr(long num, int base)
+{
+    if (num < 0)
+    {
+    	write(1, "-", 1);
+        num = -num;
+    }
+    int count = 0;
+    if (num > (base - 1))
+        count += ft_putnbr(num / base, base);
+    count += write(1, &"0123456789abcdef"[num % base], 1);
+    return count;
+}
+
+int ft_printf(const char *str, ...)
+{
+    int count = 0;
+    va_list ptr;
+    va_start(ptr, str);
+    for (; *str; str++)
+	{
+        if (*str == '%') 
+		{
+            str++;
+            if (*str == 's')
+                ft_putstr(va_arg(ptr, char*));
+            else if (*str == 'd')
+                count += ft_putnbr(va_arg(ptr, int), 10);
+            else if (*str == 'x')
+                count += ft_putnbr(va_arg(ptr, unsigned int), 16);
+        } 
+		else {
+            count += write(1, str, 1);
+        }
+    }
+    return (va_end(ptr), count);
+}
+
+int main()
+{
+    ft_printf("Merhaba, %s! Sayi: %d, Hex: %x\n", "Dunya", 42, 42);
+    return 0;
+}
