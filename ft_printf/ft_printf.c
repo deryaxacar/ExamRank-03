39e0a3c1f8b59dac46e9a4eeb6e517a173013a32
Derya Acar <118841541+deryaxacar@users.noreply.github.com>
2023-12-07 14:55:50 +0300

    Add files via upload

---
+++ ft_printf/ft_printf.c
@@ -0,0 +1,54 @@
+#include <stdarg.h>
+#include <unistd.h>
+
+void	ft_putstr(char *str)
+{
+	if (!str) str = "(null)";
+    	while (*str) write(1, str++, 1);
+}
+int ft_putnbr(long num, int base)
+{
+    int count = 0;
+    if (num < 0)
+    {
+        num *= -1;
+        count += write(1, "-", 1);
+    }
+    if (num > (base - 1))
+        count += ft_putnbr(num / base, base);
+    count += write(1, &"0123456789abcdef"[num % base], 1);
+    return count;
+}
+
+int ft_printf(const char *format, ...)
+{
+    int count = 0;
+    va_list ptr;
+    va_start(ptr, format);
+    while (*format)
+    {
+        if ((*format == '%') && (*format + 1))
+        {
+            format++;
+            if (*format == 's')
+                ft_putstr(va_arg(ptr, char*));
+            else if (*format == 'd')
+                count += ft_putnbr(va_arg(ptr, int), 10);
+            else if (*format == 'x')
+                count += ft_putnbr(va_arg(ptr, unsigned int), 16);
+        }
+        else
+            count += write(1, format, 1);
+        format++;
+    }
+    return (va_end(ptr), count);
+}
+
+int main()
+{
+	//ft_printf("%s\n", "toto");
+	//ft_printf("Magic %s is %d", "number", 42);
+	//ft_printf("Hexadecimal for %d is %x\n", 42, 42);
+	ft_printf("Merhaba, %s! Sayi: %d, Hex: %x\n", "Dunya", 42, 42);
+    return 0;
+}
