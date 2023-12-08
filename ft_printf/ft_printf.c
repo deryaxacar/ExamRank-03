04c53f51f88c92bca1c65a4d2a2fe99ca86ae68c
Derya Acar <118841541+deryaxacar@users.noreply.github.com>
2023-12-08 14:51:18 +0300

    Update ft_printf.c

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -1,47 +1,51 @@
 #include <stdarg.h>
 #include <unistd.h>
 
-void	ft_putstr(char *str)
+int      ft_putstr(char *str)
 {
-	if (!str) str = "(null)";
-    	while (*str) write(1, str++, 1);
-}
-int ft_putnbr(long num, int base)
-{
-    int count = 0;
-    if (num < 0)
-    {
-        num *= -1;
-        count += write(1, "-", 1);
-    }
-    if (num > (base - 1))
-        count += ft_putnbr(num / base, base);
-    count += write(1, &"0123456789abcdef"[num % base], 1);
-    return count;
+          int count = 0;
+          if(!str) str = "(null)";
+          while(*str) count += write(1, str++, 1);
+          return count;
+
 }
 
-int ft_printf(const char *format, ...)
+int       ft_putnbr(long num, int base)
 {
-    int count = 0;
-    va_list ptr;
-    va_start(ptr, format);
-    while (*format)
-    {
-        if ((*format == '%') && (*format + 1))
-        {
-            format++;
-            if (*format == 's')
-                ft_putstr(va_arg(ptr, char *));
-            else if (*format == 'd')
-                count += ft_putnbr(va_arg(ptr, int), 10);
-            else if (*format == 'x')
-                count += ft_putnbr(va_arg(ptr, unsigned int), 16);
-        }
-        else
-            count += write(1, format, 1);
-        format++;
-    }
-    return (va_end(ptr), count);
+          int count = 0;
+          if(num < 0)
+          {
+                    num *= -1;
+                    count += write(1, "-", 1);
+          }
+          else if(num > (base - 1))
+                    count += ft_putnbr(num / base, base);
+          count += write(1, &"0123456789abcdef"[num % base], 1);
+          return count;
+}
+
+int       ft_printf(const char *format, ...)
+{
+          int count = 0;
+          va_list ptr;
+          va_start(ptr, format);
+          while(*format)
+          {
+                    if((*format == '%') && (*format + 1))
+                    {
+                              format++;
+                              if(*format == 's')
+                                        count += ft_putstr(va_arg(ptr, char *));
+                              else if(*format == 'd')
+                                        count += ft_putnbr(va_arg(ptr, int), 10);
+                              else if(*format == 'x')
+                                        count += ft_putnbr(va_arg(ptr, unsigned int), 16);
+                    }
+                    else
+                              count += write(1, format, 1);
+                    format++;
+          }
+          return(va_end(ptr), count);
 }
 
 int main()
